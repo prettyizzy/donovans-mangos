c1092c88d57d1f2d4d05f13183a3efd5ca657478
Isabella Davis <isagdavis@outlook.com>
2024-07-22 16:03:33 -0400

    adds more starter files

---
+++ donovans-mangos/src/main/java/com/donovansmangos/donovans_mangos/DonovansMangosApplication.java
@@ -0,0 +1,13 @@
+package com.donovansmangos.donovans_mangos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonovansMangosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonovansMangosApplication.class, args);
+	}
+
+}
---
+++ donovans-mangos/src/test/java/com/donovansmangos/donovans_mangos/DonovansMangosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donovansmangos.donovans_mangos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonovansMangosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
